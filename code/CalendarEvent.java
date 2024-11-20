559becf8fe7d019ed725fac936fec9ac0f59cd4a
Alex <lxhaskins@gmail.com>
2024-11-20 12:42:14 -0800

    Calendar in creation, testing functionality

--- code/CalendarEvent.java
+++ code/CalendarEvent.java
@@ -2,18 +2,21 @@ package code;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class CalendarEvent implements Serializable {
-    private String description;
-    private String classNumber; // Associated class
+    private String description; // Description of the event
+    private String classNumber; // Class number associated with the event
     private LocalDateTime dateTime; // Date and time of the event
 
+    // Constructor
     public CalendarEvent(String description, String classNumber, LocalDateTime dateTime) {
         this.description = description;
         this.classNumber = classNumber;
         this.dateTime = dateTime;
     }
 
+    // Getters
     public String getDescription() {
         return description;
     }
@@ -26,8 +29,10 @@ public class CalendarEvent implements Serializable {
         return dateTime;
     }
 
+    // toString method to format the output without seconds
     @Override
     public String toString() {
-        return dateTime + " | " + classNumber + " | " + description;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return dateTime.format(formatter) + " | " + classNumber + " | " + description;
     }
 }
